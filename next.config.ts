32f5e5e9879ab81b0cecbc6857aef8bf5ac97a62
76ff664ptn-creator <76ff664ptn@privaterelay.appleid.com>
2026-05-13 09:51:55 +0200

    fixing redirect issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Remove allowedDevOrigins for production deployment
+  //allowedDevOrigins: ['whspr.site'],
 };
 
 export default nextConfig;
